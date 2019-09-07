9c83bcbf2b32aedddbda14018a79f31143575c91
novozhilov-a <33873613+novozhilov-a@users.noreply.github.com>
2019-09-07 20:04:22 +0300

    -перенес тесты в свою директорию -исправил локаль в тестах (.->,)

--- src/expenseReport/ExpenseReport.java
+++ src/expenseReport/ExpenseReport.java
@@ -3,46 +3,48 @@ package expenseReport;
 import java.util.ArrayList;
 import java.util.List;
 
-import static expenseReport.Expense.Type.*;
+import static expenseReport.Expense.Type.BREAKFAST;
+import static expenseReport.Expense.Type.DINNER;
 
 
-public class ExpenseReport {
-  private List<Expense> expenses = new ArrayList<Expense>();
+class ExpenseReport {
+    private List<Expense> expenses = new ArrayList<>();
 
-  public void printReport(ReportPrinter printer) {
-    int total = 0;
-    int mealExpenses = 0;
+    void printReport(ReportPrinter printer) {
+        int total = 0;
+        int mealExpenses = 0;
 
-    printer.print("Expenses " + getDate() + "\n");
 
-    for (Expense expense : expenses) {
-      if (expense.type == BREAKFAST || expense.type == DINNER)
-        mealExpenses += expense.amount;
+        printer.print("Expenses " + getDate() + "\n");
 
-      String name = "TILT";
-      switch (expense.type) {
-        case DINNER: name = "Dinner"; break;
-        case BREAKFAST: name = "Breakfast"; break;
-        case CAR_RENTAL: name = "Car Rental"; break;
-      }
-      printer.print(String.format("%s\t%s\t$%.02f\n",
-        (  (expense.type == DINNER && expense.amount > 5000)
-        || (expense.type == BREAKFAST && expense.amount > 1000)) ? "X" : " ",
-        name, expense.amount / 100.0));
+        for (Expense expense : expenses) {
+            if (expense.type == BREAKFAST || expense.type == DINNER)
+                mealExpenses += expense.amount;
 
-      total += expense.amount;
+            String name = "TILT";
+            switch (expense.type) {
+                case DINNER: name = "Dinner"; break;
+                case BREAKFAST: name = "Breakfast"; break;
+                case CAR_RENTAL: name = "Car Rental"; break;
+            }
+            printer.print(String.format("%s\t%s\t$%.02f\n",
+                    (  (expense.type == DINNER && expense.amount > 5000)
+                            || (expense.type == BREAKFAST && expense.amount > 1000)) ? "X" : " ",
+                    name, expense.amount / 100.0));
+
+            total += expense.amount;
+        }
+
+        printer.print(String.format("\nMeal expenses $%.02f",mealExpenses / 100.0 ));
+        printer.print(String.format("\nTotal $%.02f", total / 100.0));
     }
 
-    printer.print(String.format("\nMeal expenses $%.02f",mealExpenses / 100.0 ));
-    printer.print(String.format("\nTotal $%.02f", total / 100.0));
-  }
+    void addExpense(Expense expense) {
+        expenses.add(expense);
+    }
 
-  public void addExpense(Expense expense) {
-    expenses.add(expense);
-  }
-
-  private String getDate() {
-    return "9/12/2002";
-  }
+    private String getDate() {
+        return "9/12/2002";
+    }
 
 }
